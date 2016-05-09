3dc9d6b4e88959f9986a1795cc4eec6b99637d7c
stfx <stfx@hotmail.de>
2016-05-09 20:24:37 +0200

    Tab to 4 spaces

--- ThreadPool.h
+++ ThreadPool.h
@@ -16,154 +16,154 @@
 class ThreadPool
 {
 public:
-	explicit ThreadPool(int threadCount) :
-		_jobsLeft(0),
-		_bailout(false)
-	{
-		_threads = std::vector<std::thread>(threadCount);
+    explicit ThreadPool(int threadCount) :
+        _jobsLeft(0),
+        _bailout(false)
+    {
+        _threads = std::vector<std::thread>(threadCount);
 
-		for (int index = 0; index < threadCount; ++index)
-		{
-			_threads[index] = std::move(std::thread([this]
-				{
-					this->Task();
-				}));
-		}
-	}
+        for (int index = 0; index < threadCount; ++index)
+        {
+            _threads[index] = std::move(std::thread([this]
+                {
+                    this->Task();
+                }));
+        }
+    }
 
-	/**
-	 *  JoinAll on deconstruction
-	 */
-	~ThreadPool()
-	{
-		JoinAll();
-	}
+    /**
+     *  JoinAll on deconstruction
+     */
+    ~ThreadPool()
+    {
+        JoinAll();
+    }
 
-	/**
-	 *  Add a new job to the pool. If there are no jobs in the queue,
-	 *  a thread is woken up to take the job. If all threads are busy,
-	 *  the job is added to the end of the queue.
-	 */
-	void AddJob(std::function<void(void)> job)
-	{
-		// scoped lock
-		{
-			std::lock_guard<std::mutex> lock(_queueMutex);
-			_queue.emplace(job);
-		}
-		// scoped lock
-		{
-			std::lock_guard<std::mutex> lock(_jobsLeftMutex);
-			++_jobsLeft;
-		}
-		_jobAvailableVar.notify_one();
-	}
+    /**
+     *  Add a new job to the pool. If there are no jobs in the queue,
+     *  a thread is woken up to take the job. If all threads are busy,
+     *  the job is added to the end of the queue.
+     */
+    void AddJob(std::function<void(void)> job)
+    {
+        // scoped lock
+        {
+            std::lock_guard<std::mutex> lock(_queueMutex);
+            _queue.emplace(job);
+        }
+        // scoped lock
+        {
+            std::lock_guard<std::mutex> lock(_jobsLeftMutex);
+            ++_jobsLeft;
+        }
+        _jobAvailableVar.notify_one();
+    }
 
-	/**
-	 *  Join with all threads. Block until all threads have completed.
-	 *  The queue may be filled after this call, but the threads will
-	 *  be done. After invoking `ThreadPool::JoinAll`, the pool can no
-	 *  longer be used.
-	 */
-	void JoinAll()
-	{
-		// scoped lock
-		{
-			std::lock_guard<std::mutex> lock(_queueMutex);
-			if (_bailout)
-			{
-				return;
-			}
-			_bailout = true;
-		}
+    /**
+     *  Join with all threads. Block until all threads have completed.
+     *  The queue may be filled after this call, but the threads will
+     *  be done. After invoking `ThreadPool::JoinAll`, the pool can no
+     *  longer be used.
+     */
+    void JoinAll()
+    {
+        // scoped lock
+        {
+            std::lock_guard<std::mutex> lock(_queueMutex);
+            if (_bailout)
+            {
+                return;
+            }
+            _bailout = true;
+        }
 
-		// note that we're done, and wake up any thread that's
-		// waiting for a new job
-		_jobAvailableVar.notify_all();
+        // note that we're done, and wake up any thread that's
+        // waiting for a new job
+        _jobAvailableVar.notify_all();
 
-		for (auto& x : _threads)
-		{
-			if (x.joinable())
-			{
-				x.join();
-			}
-		}
-	}
+        for (auto& x : _threads)
+        {
+            if (x.joinable())
+            {
+                x.join();
+            }
+        }
+    }
 
-	/**
-	 *  Wait for the pool to empty before continuing.
-	 *  This does not call `std::thread::join`, it only waits until
-	 *  all jobs have finished executing.
-	 */
-	void WaitAll()
-	{
-		std::unique_lock<std::mutex> lock(_jobsLeftMutex);
-		if (_jobsLeft > 0)
-		{
-			_waitVar.wait(lock, [this]
-			              {
-				              return _jobsLeft == 0;
-			              });
-		}
-	}
+    /**
+     *  Wait for the pool to empty before continuing.
+     *  This does not call `std::thread::join`, it only waits until
+     *  all jobs have finished executing.
+     */
+    void WaitAll()
+    {
+        std::unique_lock<std::mutex> lock(_jobsLeftMutex);
+        if (_jobsLeft > 0)
+        {
+            _waitVar.wait(lock, [this]
+                          {
+                              return _jobsLeft == 0;
+                          });
+        }
+    }
 
 private:
-	/**
-	 *  Take the next job in the queue and run it.
-	 *  Notify the main thread that a job has completed.
-	 */
-	void Task()
-	{
-		while (true)
-		{
-			std::function<void(void)> job;
+    /**
+     *  Take the next job in the queue and run it.
+     *  Notify the main thread that a job has completed.
+     */
+    void Task()
+    {
+        while (true)
+        {
+            std::function<void(void)> job;
 
-			// scoped lock
-			{
-				std::unique_lock<std::mutex> lock(_queueMutex);
+            // scoped lock
+            {
+                std::unique_lock<std::mutex> lock(_queueMutex);
 
-				if (_bailout)
-				{
-					return;
-				}
+                if (_bailout)
+                {
+                    return;
+                }
 
-				// Wait for a job if we don't have any.
-				_jobAvailableVar.wait(lock, [this]
-				                      {
-					                      return _queue.size() > 0 || _bailout;
-				                      });
+                // Wait for a job if we don't have any.
+                _jobAvailableVar.wait(lock, [this]
+                                      {
+                                          return _queue.size() > 0 || _bailout;
+                                      });
 
-				if (_bailout)
-				{
-					return;
-				}
+                if (_bailout)
+                {
+                    return;
+                }
 
-				// Get job from the queue
-				job = _queue.front();
-				_queue.pop();
-			}
+                // Get job from the queue
+                job = _queue.front();
+                _queue.pop();
+            }
 
-			job();
+            job();
 
-			// scoped lock
-			{
-				std::lock_guard<std::mutex> lock(_jobsLeftMutex);
-				--_jobsLeft;
-			}
+            // scoped lock
+            {
+                std::lock_guard<std::mutex> lock(_jobsLeftMutex);
+                --_jobsLeft;
+            }
 
-			_waitVar.notify_one();
-		}
-	}
+            _waitVar.notify_one();
+        }
+    }
 
-	std::vector<std::thread> _threads;
-	std::queue<std::function<void(void)>> _queue;
+    std::vector<std::thread> _threads;
+    std::queue<std::function<void(void)>> _queue;
 
-	int _jobsLeft;
-	bool _bailout;
-	std::condition_variable _jobAvailableVar;
-	std::condition_variable _waitVar;
-	std::mutex _jobsLeftMutex;
-	std::mutex _queueMutex;
+    int _jobsLeft;
+    bool _bailout;
+    std::condition_variable _jobAvailableVar;
+    std::condition_variable _waitVar;
+    std::mutex _jobsLeftMutex;
+    std::mutex _queueMutex;
 };
 
 #endif //CONCURRENT_THREADPOOL_H
